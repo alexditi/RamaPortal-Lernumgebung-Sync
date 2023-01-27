6af3044c5e69112197343b52a730a87447219b8a
Alex Ditinger <67831707+alexditi@users.noreply.github.com>
2023-01-27 14:35:33 +0100

    Update get_guid.cmd

--- Aufgabenplanung Vorlage/get_guid.cmd
+++ Aufgabenplanung Vorlage/get_guid.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-SET SearchString="'Name'^^^>DitingerWLAN"
+SET SearchString="'Name'^^^>NetworkName"
 FOR /f "delims=" %%i IN ('wevtutil qe Microsoft-Windows-NetworkProfile/Operational /q:"Event[System[(EventID=10000)]]" /c:100 /rd:true /f:xml ^| FINDSTR /R "%SearchString%"') DO (
 ECHO %%i
 )
